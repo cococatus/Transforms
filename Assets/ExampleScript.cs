0bbf36dd0ff6cc2c240f16ca7ec25df9b8e28746
zhanna <jeannette.drobina@gmail.com>
2019-08-08 23:35:05 +0300

    quaternions

---
+++ Assets/ExampleScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+// Transform.Rotate example
+//
+// Two cubes are created.  One (red) is rendered using Space.Self.  The
+// other (green) uses Space.World.  The rotation is controlled using xAngle,
+// yAngle and zAngle. Over time, the cubes rotate differently.
+
+public class ExampleScript : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    public Material selfMat, worldMat;
+
+    private GameObject cube1, cube2;
+
+    void Awake()
+    {
+        cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1.transform.position = new Vector3(0.75f, 0.0f, 0.0f);
+        cube1.GetComponent<Renderer>().material = selfMat;
+        cube1.name = "Self";
+
+        cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube2.transform.position = new Vector3(-0.75f, 0.0f, 0.0f);
+        cube2.GetComponent<Renderer>().material = worldMat;
+        cube2.name = "World";
+        
+//        cube1.transform.rotation = Quaternion.AngleAxis(45, Vector3.forward);
+//        cube1.transform.rotation = Quaternion.AngleAxis(45, Vector3.right);
+//        cube1.transform.rotation = Quaternion.AngleAxis(45, Vector3.up);
+    }
+
+//    void Update()
+//    {
+//        cube1.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+//        cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+//    }
+}
---
+++ Assets/RotationQuaternionEx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RotationMethod
+{
+    LookRotation,
+    Angle,
+    Euler,
+    Slerp,
+    FromToRotation,
+    identity
+}
+public class RotationQuaternionEx : MonoBehaviour
+{
+    public RotationMethod rotationMethod = RotationMethod.identity;
+    
+    public Transform target;
+    
+    public Transform from;
+    public Transform to;
+    public float speed = 0.1F;
+    
+    void Update()
+    {
+        Quaternion rotation;
+        switch (rotationMethod)
+        {
+            case RotationMethod.LookRotation:
+                Vector3 relativePos = target.position - transform.position;
+                target.transform.rotation = Quaternion.LookRotation(relativePos);
+                break;
+            case RotationMethod.Angle:
+                float angle = Quaternion.Angle(transform.rotation, target.rotation);
+                Debug.Log(angle);
+                break;
+            case RotationMethod.Euler:
+                target.transform.rotation = Quaternion.Euler(new Vector3(0, 30, 0));
+                break;
+            case RotationMethod.Slerp:
+                target.transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, Time.time * speed);
+                break;
+            case RotationMethod.FromToRotation:
+                target.transform.rotation = Quaternion.FromToRotation(Vector3.up, transform.forward);
+                break;
+            case RotationMethod.identity:
+                target.transform.rotation = Quaternion.identity;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+    }
+}
